e27569072cb60ed7bd3d1b47916b1cf713412ac0
Felipe de Souza <de.souza258@gmail.com>
2018-11-14 16:51:28 +0100

    :exploding_head:

---
+++ main.js
@@ -0,0 +1,44 @@
+var portFromCS;
+var triggerUrl = "https://web.cached.id/end"
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onFinishExperience(tabInfo) {
+	console.log(tabInfo);
+}
+
+function connected(p) {
+  portFromCS = p;
+  portFromCS.onMessage.addListener(function(m) {
+
+    if(m.location == triggerUrl) {
+    	
+
+  		browser.tabs.query({active: true})
+		  .then(tabs => browser.tabs.get(tabs[0].id))
+		  .then(tab => {
+		    console.info(tab);
+		    var id = tab.id;
+
+		    var newCached = browser.tabs.create({
+    			url:"https://web.cached.id"
+  			});
+
+  			newCached.then(function() {
+  				var removing = browser.tabs.remove(id);
+				removing.then(function() {
+					browser.browsingData.removeCookies({});
+				}, onError);
+  			}, onError)
+		  });
+
+
+  		
+    }
+
+  });
+}
+
+browser.runtime.onConnect.addListener(connected);
