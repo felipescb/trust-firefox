4d30fed1d1284607f60a64243eee317d93c6a781
Felipe <de.souza258@gmail.com>
2019-11-11 13:39:25 -0200

    piksel.xpi

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var portFromCS;
-var triggerUrl = "https://va.cached.id/end"
+var triggerUrl = "https://piksel.cached.id/end"
 
 function onError(error) {
   console.log(`Error: ${error}`);
@@ -23,7 +23,7 @@ function connected(p) {
 		    var id = tab.id;
 
 		    var newCached = browser.tabs.create({
-    			url:"https://va.cached.id"
+    			url:"https://piksel.cached.id"
   			});
 
   			newCached.then(function() {
